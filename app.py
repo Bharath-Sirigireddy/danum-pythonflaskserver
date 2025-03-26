be109a2da5029fb641531733ab497abc7e2d2b4a
Bharath-Sirigireddy <bharathsirigireddy03@gmail.com>
2025-03-26 14:31:31 +0530

    added cart route into app.py

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask("My Flask Application")
 def hello():
     return "<h1>My First Hello World!</h1>"
 
+@app.route("/cart")
+def shopping_cart():
+    return "<h2>Shopping Cart</h2><p>Your cart is currently empty.</p>"
+
 if __name__=="__main__":
     app.run(debug=True) 
     # When no port is specified, starts at default port 5000
